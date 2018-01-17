80281eb7940f349535a337706633b6a6708cbc09
igor <bujhm1313@gmail.com>
2018-01-17 14:39:14 +0200

    [IMP] Add callback post method handler

--- small_test_server.py
+++ small_test_server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 
@@ -6,6 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/callback_route', methods=['POST'])
+def callback():
+    data = request.data
+    json = request.json
+
+    with open('{}/dump.txt'.format(app.root_path), 'w') as file:
+        file.write(str(data))
+        file.write(str(json))
+
+    return 'ok'
 
 
 if __name__ == '__main__':
